5f98460c4608459e84d0779356ca54a979a64f84
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-10-25 23:49:44 +0530

    04 - Dependency Injection and DBContext

--- MagicVilla_API/Model/Dto/VillaDTO.cs
+++ MagicVilla_API/Model/Dto/VillaDTO.cs
@@ -5,12 +5,13 @@ namespace MagicVilla_API.Model.Dto
     public class VillaDTO
     {
         public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Details { get; set; }
+        public required double Rate { get; set; }
+        public required int Sqft { get; set; }
+        public required int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Amenity { get; set; }
 
-        [MaxLength(30)]
-        public required  string Name { get; set; }
-
-        public int Occupancy { get; set; }
-
-        public int Sqft { get; set; }
     }
 }
